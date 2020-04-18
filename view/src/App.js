f6d0375fe6242d4610fb1b94aa879db38057c4d1
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-04-18 11:10:54 -0500

    material ui setup

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+
 function App() {
   return (
     <div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={login} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
